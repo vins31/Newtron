05c587ae7a9b521071267a0ada44548d7a7da755
Guillaume Babin <645219+gbabin@users.noreply.github.com>
2011-03-01 22:50:08 +0100

    fix header

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,34 +1,26 @@
 /*
-	This file is part of Newtron.
+    Newtron
+    Copyright © 2011 Rémi Palandri, Vincent A., Guillaume Babin
+    This file is part of Newtron.
 
     Newtron is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as 
     published by the Free Software Foundation, either version 3 of the 
     License, or (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    Newtron is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public 
-    License along with Foobar.  
+    License along with Newtron.  
     If not, see <http://www.gnu.org/licenses/>.
 */
-Classe
-variable
 
-if() {
-	
-} else {
-	
-}
-
-if () 
+#include <iostream>
+ 
+int main()
 {
-	
-} 
-else 
-{
-	
+    std::cout << "Hello, world!\n";
 }
