1846cec63d47e7ce033d45bb88f52e6daf623c1b
Guillaume Babin <645219+gbabin@users.noreply.github.com>
2011-03-01 22:52:46 +0100

    fix utf8

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,25 +1,25 @@
 /*
     Newtron
-    Copyright © 2011 Rémi Palandri, Vincent A., Guillaume Babin
+    Copyright Â© 2011 RÃ©mi Palandri, Vincent A., Guillaume Babin
     This file is part of Newtron.
 
     Newtron is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as 
-    published by the Free Software Foundation, either version 3 of the 
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
     License, or (at your option) any later version.
 
     Newtron is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
-    License along with Newtron.  
+    You should have received a copy of the GNU Lesser General Public
+    License along with Newtron.
     If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
- 
+
 int main()
 {
     std::cout << "Hello, world!\n";
